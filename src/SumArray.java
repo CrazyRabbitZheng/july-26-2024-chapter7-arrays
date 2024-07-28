b1695cc4c5a09611207fbfec7bc79f4fc457c0ef
CrazyRabbitZheng <alicejrha@gmail.com>
2024-07-28 15:06:01 -0230

    update

--- src/SumArray.java
+++ src/SumArray.java
@@ -6,9 +6,14 @@ public class SumArray {
     public static void main(String[] args) {
         int[] arr = { 87,68,94,100,83,78,85,91,76,87};
         int total = 0;
+//below is traditional for loop
+//        for (int i = 0; i < arr.length; i++) {
+//            total += arr[i];
+//        }
 
-        for (int i = 0; i < arr.length; i++) {
-            total += arr[i];
+        //below is enhanced for loop -- for each
+        for(int score : arr){
+            total += score;
         }
 
         System.out.println("The sum of the array is: " + total);
